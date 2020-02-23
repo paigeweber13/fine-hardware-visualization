63331a0c4323564061875f193921b40f6f295a2a
Riley Weber <rileyw13@protonmail.com>
2020-02-23 18:54:17 -0500

    added defines for memory stuff

--- lib/architecture.h
+++ lib/architecture.h
@@ -20,4 +20,19 @@ const unsigned L3_CACHE_GROUPS [1][4] = {{0, 2, 1, 3}};
 const unsigned NUMA_DOMAINS [1][4] = {{0, 1, 2, 3}};
 
 // ---- Benchmark ---- //
+// -- computation --
 #define EXPERIENTIAL_SP_RATE_TFLOPS 0.181
+
+// -- memory -- 
+// all measurements are in GiB/s
+#define EXPERIENTIAL_READ_BW_L1  0.
+#define EXPERIENTIAL_READ_BW_L2  0.
+#define EXPERIENTIAL_READ_BW_L3  0.
+#define EXPERIENTIAL_READ_BW_RAM 0.
+
+// read/write operations (like copying) are typically limited by write and
+// typically have the same bandwidth as write operations
+#define EXPERIENTIAL_WRITE_BW_L1  0.
+#define EXPERIENTIAL_WRITE_BW_L2  0.
+#define EXPERIENTIAL_WRITE_BW_L3  0.
+#define EXPERIENTIAL_WRITE_BW_RAM 0.
