e0b8a298baf586cae05211bb74530dd7a0285bd7
Riley Weber <rileyw13@protonmail.com>
2020-03-04 19:54:31 -0500

    improved output, fixed memory stuff, separated 3 types of tests

--- tests/benchmark_comparison_test.py
+++ tests/benchmark_comparison_test.py
@@ -1,6 +1,7 @@
 import argparse
 import csv
 import subprocess
+import time
 
 benchmark_comparison_bin_location = '../bin/tests/benchmark-likwid-vs-manual'
 flops_csv = './flops_comparison.csv'
@@ -14,43 +15,60 @@ mem_csv_header = ['manual_duration','manual_data_size_mb', \
                  'likwid_data_size_mb','likwid_bandwitdh_mb_per_s']
 
 def write_csv(filename, header, data):
+    print('writing csv to ' + filename)
     with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(header)
         writer.writerows(data)
 
-def run_tests():
+def run_flop_tests():
     output = []
     num_flop_iterations = 100
-    while num_flop_iterations < 10000000001:
-        print('running flops with ' + str(num_flop_iterations) + ' iterations')
+    while num_flop_iterations < 100000000001:
+        print('running flops with ' + str(num_flop_iterations) + ' iterations',
+              end='', flush=True)
+        start_time = time.time()
         child = subprocess.run(
             [benchmark_comparison_bin_location, '-f', str(num_flop_iterations),
              '-c'], stdout=subprocess.PIPE)
+        elapsed_time = time.time() - start_time
+        print(' {:.3f}s'.format(elapsed_time))
         output.append(child.stdout.decode().strip().split(','))
         num_flop_iterations *= 10
 
     write_csv(flops_csv, flops_csv_header, output)
 
+def run_mem_iteration_tests():
     num_mem_iterations = 10
+    mem_size = 10000
     output = []
     while num_mem_iterations < 100001:
-        print('running flops with ' + str(num_flop_iterations) + ' iterations')
+        print('running memory with ' + str(num_mem_iterations) + ' iterations'\
+              ' and size ' + str(mem_size), end='', flush=True)
+        start_time = time.time()
         child = subprocess.run(
-            [benchmark_comparison_bin_location, '-f', str(num_flop_iterations),
-             '-c'], stdout=subprocess.PIPE)
+            [benchmark_comparison_bin_location, '-m', str(num_mem_iterations),
+             str(mem_size), '-c'], stdout=subprocess.PIPE)
+        elapsed_time = time.time() - start_time
+        print(' {:.3f}s'.format(elapsed_time))
         output.append(child.stdout.decode().strip().split(','))
         num_mem_iterations *= 10
 
     write_csv(mem_iter_csv, mem_csv_header, output)
 
+def run_mem_size_tests():
+    num_mem_iterations = 10
     mem_size = 5000
     output = []
     while mem_size < 1000001:
-        print('running flops with ' + str(num_flop_iterations) + ' iterations')
+        print('running memory with ' + str(num_mem_iterations) + ' iterations'\
+              ' and size ' + str(mem_size), end='', flush=True)
+        start_time = time.time()
         child = subprocess.run(
-            [benchmark_comparison_bin_location, '-f', str(num_flop_iterations),
-             '-c'], stdout=subprocess.PIPE)
+            [benchmark_comparison_bin_location, '-m', str(num_mem_iterations),
+             str(mem_size), '-c'], stdout=subprocess.PIPE)
+        elapsed_time = time.time() - start_time
+        print(' {:.3f}s'.format(elapsed_time))
         output.append(child.stdout.decode().strip().split(','))
         mem_size *= 2
 
@@ -62,16 +80,21 @@ def plot_results():
 def parse_cli_options():
     parser = argparse.ArgumentParser(
         description='Compare manual results with likwid results.')
-    parser.add_argument('--run-tests', '-r', action='store_true')
+    parser.add_argument('--run-flop-tests', '-f', action='store_true')
+    parser.add_argument('--run-mem-iteration-tests', '-i', action='store_true')
+    parser.add_argument('--run-mem-size-tests', '-s', action='store_true')
     parser.add_argument('--plot-results', '-p', action='store_true')
 
     return parser.parse_args()
 
 def main():
-    write_csv(flops_csv, flops_csv_header, [])
     args = parse_cli_options()
-    if args.run_tests:
-        run_tests()
+    if args.run_flop_tests:
+        run_flop_tests()
+    if args.run_mem_iteration_tests:
+        run_mem_iteration_tests()
+    if args.run_mem_size_tests:
+        run_mem_size_tests()
     if args.plot_results:
         plot_results()
 
