456fa2aba67a0ed94d14e984e1a559598ca7777e
Riley Weber <rileyw13@protonmail.com>
2020-03-11 17:16:39 -0400

    created skeleton for command line arguments

--- src/fhv.cpp
+++ src/fhv.cpp
@@ -3,9 +3,16 @@
 //  - create visualization from output data
 
 #include <boost/program_options.hpp>
+#include <iostream>
 
+using namespace std;
 namespace po = boost::program_options;
+
+int parse_cli_options(int argc, char *argv[])
+{
+}
 
 int main(int argc, char *argv[])
 {
+  return parse_cli_options(argc, argv);
 }
