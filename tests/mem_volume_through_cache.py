c685ee933f7c1f65e6f1caecde6624ea46d3b400
Riley Weber <rileyw13@protonmail.com>
2020-03-15 19:50:28 -0400

    added ratios, separated graphs for each level of cache/memory

--- tests/mem_volume_through_cache.py
+++ tests/mem_volume_through_cache.py
@@ -49,46 +49,116 @@ def run_tests():
 
 def plot_results():
     data =  pandas.read_csv(results_file + '.csv')
+
+    ### RAW VOLUME - L2
     plt.figure(1, figsize=(10,7))
 
     # trial_nums = range(len(data['L2D evict data volume [GBytes]']))
     plt.plot(data['Single iteration size'], 
-             data['L2D evict data volume [GBytes]'],
-             color='midnightblue')
-    plt.plot(data['Single iteration size'], 
              data['L2D load data volume [GBytes]'],
              color='cornflowerblue')
     plt.plot(data['Single iteration size'], 
-             data['L3 evict data volume [GBytes]'],
-             color='darkred')
+             data['L2D evict data volume [GBytes]'],
+             color='midnightblue')
+
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.title('Comparing read and write volumes through L2 cache')
+    plt.xlabel('Size of one iteration (KBytes)')
+    plt.ylabel('Memory volume (GBytes)')
+    plt.legend([
+        'L2D load data volume [GBytes]',
+        'L2D evict data volume [GBytes]',
+    ])
+
+    plt.savefig(results_file + '_l2.png')
+
+    ### RAW VOLUME - L3
+    plt.figure(2, figsize=(10,7))
+
+    # trial_nums = range(len(data['L2D evict data volume [GBytes]']))
     plt.plot(data['Single iteration size'], 
              data['L3 load data volume [GBytes]'],
              color='red')
     plt.plot(data['Single iteration size'], 
-             data['Memory evict data volume [GBytes]'],
-             color='darkgreen')
-    plt.plot(data['Single iteration size'], 
-             data['Memory load data volume [GBytes]'],
-             color='seagreen')
-    # print(data)
-    # print(data['Memory evict data volume [GBytes]'])
-    # print(data['Memory load data volume [GBytes]'])
+             data['L3 evict data volume [GBytes]'],
+             color='darkred')
 
-    # plt.xscale('log')
-    # plt.yscale('log')
-    plt.title('Comparing read and write volumes through cache and memory')
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.title('Comparing read and write volumes through L3 cache')
     plt.xlabel('Size of one iteration (KBytes)')
     plt.ylabel('Memory volume (GBytes)')
     plt.legend([
-        'L2D evict data volume [GBytes]',
-        'L2D load data volume [GBytes]',
-        'L3 evict data volume [GBytes]',
         'L3 load data volume [GBytes]',
-        'Memory evict data volume [GBytes]',
-        'Memory load data volume [GBytes]',
+        'L3 evict data volume [GBytes]',
     ])
 
-    plt.savefig(results_file + '.png')
+    plt.savefig(results_file + '_l3.png')
+    
+    ### RAW VOLUME - Memory
+    plt.figure(3, figsize=(10,7))
+
+    plt.plot(data['Single iteration size'], 
+             data['Memory load data volume [GBytes]'],
+             color='seagreen')
+    plt.plot(data['Single iteration size'], 
+             data['Memory evict data volume [GBytes]'],
+             color='darkgreen')
+
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.title('Comparing read and write volumes through memory')
+    plt.xlabel('Size of one iteration (KBytes)')
+    plt.ylabel('Memory volume (GBytes)')
+    plt.legend([
+        'Memory load data volume [GBytes]',
+        'Memory evict data volume [GBytes]',
+    ])
+
+    plt.savefig(results_file + '_memory.png')
+
+    ### RATIOS
+    plt.figure(4, figsize=(10,7))
+    l2_ratios = data['L2D load data volume [GBytes]'] \
+                / data['L2D evict data volume [GBytes]']
+    l3_ratios = data['L3 load data volume [GBytes]'] \
+                / data['L3 evict data volume [GBytes]']
+    mem_ratios = data['Memory load data volume [GBytes]'] \
+                 / data['Memory evict data volume [GBytes]']
+
+    # cap ratios
+    max_ratio = 4
+    l2_ratios = l2_ratios.clip(0,max_ratio)
+    l3_ratios = l3_ratios.clip(0,max_ratio)
+    mem_ratios = mem_ratios.clip(0,max_ratio)
+
+
+    # print(data['L2D load data volume [GBytes]'])
+    # print(data['L2D evict data volume [GBytes]'])
+    # print('l2 ratios: \n', l2_ratios)
+    # print('l3 ratios: \n', l3_ratios)
+    # print('mem ratios: \n', mem_ratios)
+    print('NOTE: ratios are capped at {:d}. Some are very large (in the ' \
+          'order of 10^2) but are capped for visibility'.format(max_ratio))
+
+    plt.plot(data['Single iteration size'], l2_ratios,
+             color='midnightblue')
+    plt.plot(data['Single iteration size'], l3_ratios,
+             color='darkred')
+    plt.plot(data['Single iteration size'], mem_ratios,
+             color='darkgreen')
+
+    plt.title('Comparing ratios of reads:writes through cache and memory')
+    plt.xlabel('Size of one iteration (KBytes)')
+    plt.ylabel('Ratio of read:write volumes')
+    plt.legend([
+        'L2',
+        'L3',
+        'Memory',
+    ])
+
+    plt.savefig(results_file + '_ratios.png')
 
     plt.show()
 
