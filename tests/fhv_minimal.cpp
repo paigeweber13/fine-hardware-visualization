432398714f7c64913c377258108fa083680068ff
Riley Weber <rileyw13@protonmail.com>
2020-04-09 19:15:29 -0400

    trying to isolate why init doesn't work

--- tests/fhv_minimal.cpp
+++ tests/fhv_minimal.cpp
@@ -1,3 +1,5 @@
+// does not yet work
+
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,10 +17,27 @@ int main()
   printf("\n\nThis is a minimal example of how the fhv performance_monitor "
          "api works\n");
 
-  performance_monitor::init(
-    // "MEM_DP|FLOPS_SP|L3|L2|PORT_USAGE1|PORT_USAGE2|PORT_USAGE3",
-    "MEM_DP|FLOPS_SP|L3|L2",
-    "double_flops,copy", "");
+  // ---- performance_monitor initialization (currently broken)
+  // performance_monitor::init(
+  //   "MEM|FLOPS_DP|L3|L2|PORT_USAGE1|PORT_USAGE2|PORT_USAGE3",
+  //   // "MEM|FLOPS_DP|L3|L2",
+  //   "double_flops,copy", "");
+
+  // ---- begin likwid initialization
+
+  performance_monitor::setEnvironmentVariables(
+    "MEM|FLOPS_DP|L3|L2|PORT_USAGE1|PORT_USAGE2|PORT_USAGE3");
+  likwid_markerInit();
+
+#pragma omp parallel
+  {
+    likwid_markerThreadInit();
+    likwid_markerRegisterRegion("double_flops");
+    likwid_markerRegisterRegion("copy");
+  }
+
+  perfmon_startCounters();
+  // ---- end likwid initialization
 
   double a, b, c;
   a = 1.8;
@@ -29,12 +48,12 @@ int main()
   double arr[n];
   double copy_arr[n];
 
-// perfmon_startCounters();
 #pragma omp parallel
   {
     for (int j = 0; j < 8; j++)
     {
       printf("thread %d, iteration %d\n", omp_get_thread_num(), j);
+      // likwid_markerStartRegion("double_flops");
       performance_monitor::startRegion("double_flops");
 #pragma omp barrier
       for (int i = 0; i < 10000000; i++)
@@ -43,22 +62,26 @@ int main()
       }
 #pragma omp barrier
       performance_monitor::stopRegion("double_flops");
-#pragma omp barrier
       performance_monitor::startRegion("copy");
-#pragma omp barrier
+      // likwid_markerStopRegion("double_flops");
+      // likwid_markerStartRegion("copy");
       for (int i = 0; i < 1000; i++){
         copy(arr, copy_arr, n);
       }
 #pragma omp barrier
       performance_monitor::stopRegion("copy");
-#pragma omp barrier
       performance_monitor::nextGroup();
+      // likwid_markerStopRegion("copy");
+      // likwid_markerNextGroup();
     }
   }
 
   printf("final c: %f\n", c);
+  printf("final random part of copy_arr: %f\n", 
+         copy_arr[( (size_t) c ) % n]);
 
   performance_monitor::close();
+  // currently broken
   performance_monitor::printOnlyAggregate();
   performance_monitor::printComparison();
 }
