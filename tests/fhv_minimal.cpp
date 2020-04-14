e79003149c84ccc24deb271e0f66b99560456e60
Riley Weber <rileyw13@protonmail.com>
2020-04-13 20:45:06 -0400

    now only uses fhv to print aggregate data. Does nothing to wrap likwid calls

--- tests/fhv_minimal.cpp
+++ tests/fhv_minimal.cpp
@@ -14,31 +14,33 @@ void copy(double * arr, double * copy_arr, size_t n){
 
 int main()
 {
-  printf("\n\nThis is a minimal example of how the fhv performance_monitor "
-         "api works\n");
-
-  // ---- performance_monitor initialization
-  performance_monitor::init(
-    // "MEM|FLOPS_DP|L3|L2|PORT_USAGE1|PORT_USAGE2|PORT_USAGE3",
-    "MEM|FLOPS_DP|L3|L2",
-    "double_flops,copy", "");
+  printf("\n\nThis is a minimal example of how the fhv performance_monitor \n"
+         "aggregation prints work\n");
 
   // ---- begin likwid initialization
 
-  // performance_monitor::setEnvironmentVariables(
-  //   "MEM|FLOPS_DP|L3|L2",
-  //   "0,1,2,3");
-  //   "MEM|FLOPS_DP|L3|L2|PORT_USAGE1|PORT_USAGE2|PORT_USAGE3");
+  const char *filepath = performance_monitor::likwidOutputFilepath.c_str();
 
-//   likwid_markerInit();
+  // so 14 group/region combos
+  setenv("LIKWID_EVENTS",
+         "MEM|L2|L3|FLOPS_DP|PORT_USAGE1|PORT_USAGE2|PORT_USAGE3",
+         1);
+  // setenv("LIKWID_EVENTS", "MEM_DP|L2", 1);
+  setenv("LIKWID_MODE", "1", 1);
+  // output filepath
+  setenv("LIKWID_FILEPATH", filepath, 1); 
+  setenv("LIKWID_THREADS", "0,1,2,3", 1); // list of threads
+  setenv("LIKWID_FORCE", "1", 1);
 
-// #pragma omp parallel
-//   {
-//     likwid_markerThreadInit();
-//     likwid_markerRegisterRegion("double_flops");
-//     likwid_markerRegisterRegion("copy");
-//     likwid_pinThread(omp_get_thread_num());
-//   }
+  likwid_markerInit();
+
+#pragma omp parallel
+  {
+    likwid_markerThreadInit();
+    likwid_markerRegisterRegion("double_flops");
+    likwid_markerRegisterRegion("copy");
+    likwid_pinThread(omp_get_thread_num());
+  }
 
   // ---- end likwid initialization
 
@@ -56,26 +58,21 @@ int main()
     for (int j = 0; j < 8; j++)
     {
       printf("thread %d, iteration %d\n", omp_get_thread_num(), j);
-      // likwid_markerStartRegion("double_flops");
-      performance_monitor::startRegion("double_flops");
+      likwid_markerStartRegion("double_flops");
 #pragma omp barrier
       for (int i = 0; i < 10000000; i++)
       {
         c = a * b + c;
       }
 #pragma omp barrier
-      performance_monitor::stopRegion("double_flops");
-      performance_monitor::startRegion("copy");
-      // likwid_markerStopRegion("double_flops");
-      // likwid_markerStartRegion("copy");
+      likwid_markerStopRegion("double_flops");
+      likwid_markerStartRegion("copy");
       for (int i = 0; i < 1000; i++){
         copy(arr, copy_arr, n);
       }
 #pragma omp barrier
-      performance_monitor::stopRegion("copy");
-      performance_monitor::nextGroup();
-      // likwid_markerStopRegion("copy");
-      // likwid_markerNextGroup();
+      likwid_markerStopRegion("copy");
+      likwid_markerNextGroup();
     }
   }
 
@@ -83,9 +80,14 @@ int main()
   printf("final random part of copy_arr: %f\n", 
          copy_arr[( (size_t) c ) % n]);
 
-  // likwid_markerClose();
-  performance_monitor::close();
+  likwid_markerClose();
+
+  // performance_monitor::printRegionGroupEventAndMetricData();
   // performance_monitor::printDetailedResults();
+
+  performance_monitor::getAggregateResults();
   performance_monitor::printOnlyAggregate();
+
+  performance_monitor::compareActualWithBench();
   performance_monitor::printComparison();
 }
