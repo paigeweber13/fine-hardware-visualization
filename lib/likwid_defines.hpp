2f8f911c65f6adc23885302467e01929077701e8
Riley Weber <rileyw13@protonmail.com>
2020-04-23 01:34:38 -0400

    moved defines to separate file

---
+++ lib/likwid_defines.hpp
@@ -0,0 +1,57 @@
+// JUSTIFICATION: using defines for these things turns incorrect spellings into
+// compile-time errors instead of runtime errors. This is good!
+
+// group names
+#define likwid_group_flops_sp "FLOPS_SP"
+#define likwid_group_flops_dp "FLOPS_DP"
+#define likwid_group_l2 "L2"
+#define likwid_group_l3 "L3"
+#define likwid_group_mem "MEM"
+#define likwid_group_port1 "PORT_USAGE1"
+#define likwid_group_port2 "PORT_USAGE2"
+#define likwid_group_port3 "PORT_USAGE3"
+
+// number of flops
+#define sp_scalar_flops_event_name "FP_ARITH_INST_RETIRED_SCALAR_SINGLE"
+#define sp_avx_128_flops_event_name "FP_ARITH_INST_RETIRED_128B_PACKED_SINGLE"
+#define sp_avx_256_flops_event_name "FP_ARITH_INST_RETIRED_256B_PACKED_SINGLE"
+
+// flop rates
+#define mflops_metric_name "SP [MFLOP/s]"
+#define mflops_dp_metric_name "DP [MFLOP/s]"
+
+// port usage ratios
+#define port0_usage_ratio "Port0 usage ratio"
+#define port1_usage_ratio "Port1 usage ratio"
+#define port2_usage_ratio "Port2 usage ratio"
+#define port3_usage_ratio "Port3 usage ratio"
+#define port4_usage_ratio "Port4 usage ratio"
+#define port5_usage_ratio "Port5 usage ratio"
+#define port6_usage_ratio "Port6 usage ratio"
+#define port7_usage_ratio "Port7 usage ratio"
+
+// cache volume and bandwidth
+#define l2_bandwidth_metric_name "L2 bandwidth [MBytes/s]"
+#define l2_data_volume_name "L2 data volume [GBytes]"
+#define l2_evict_bandwidth_name "L2D evict bandwidth [MBytes/s]"
+#define l2_evict_data_volume_name "L2D evict data volume [GBytes]"
+#define l2_load_bandwidth_name "L2D load bandwidth [MBytes/s]"
+#define l2_load_data_volume_name "L2D load data volume [GBytes]"
+
+#define l3_bandwidth_metric_name "L3 bandwidth [MBytes/s]"
+#define l3_data_volume_name "L3 data volume [GBytes]"
+#define l3_evict_bandwidth_name "L3 evict bandwidth [MBytes/s]"
+#define l3_evict_data_volume_name "L3 evict data volume [GBytes]"
+#define l3_load_bandwidth_name "L3 load bandwidth [MBytes/s]"
+#define l3_load_data_volume_name "L3 load data volume [GBytes]"
+
+// memory volume and bandwidth
+#define ram_bandwidth_metric_name "Memory bandwidth [MBytes/s]"
+#define ram_data_volume_metric_name "Memory data volume [GBytes]"
+#define ram_evict_bandwidth_name "Memory evict bandwidth [MBytes/s]"
+#define ram_evict_data_volume_name "Memory evict data volume [GBytes]"
+#define ram_load_bandwidth_name "Memory load bandwidth [MBytes/s]"
+#define ram_load_data_volume_name "Memory load data volume [GBytes]"
+
+// other memroy/cache stuff
+#define load_to_store_ratio_metric_name "Load to store ratio"
--- lib/performance_monitor.h
+++ lib/performance_monitor.h
@@ -14,6 +14,7 @@
 #include <string>
 
 #include "architecture.h"
+#include "likwid_defines.hpp"
 
 // magic numbers
 #define ACCESSMODE_DAEMON "1"
@@ -22,70 +23,12 @@
 #define OPS_PER_SP_256_VECTOR 8
 #define OPS_PER_SP_128_VECTOR 4
 
-// ---- Names of things ---- //
-
-// JUSTIFICATION: using defines for these things turns incorrect spellings into
-// compile-time errors instead of runtime errors. This is good!
-
-// group names
-#define likwid_group_flops_sp "FLOPS_SP"
-#define likwid_group_flops_dp "FLOPS_DP"
-#define likwid_group_l2 "L2"
-#define likwid_group_l3 "L3"
-#define likwid_group_mem "MEM"
-
-// custom keywords
+// keywords
+#define total_sp_flops_event_name "total sp flops"
 // #define group_sum_by_region_keyword "group_sum_by_region"
 #define all_groups_keyword "all_groups"
 #define all_regions_keyword "all_regions"
 
-// number of flops
-#define total_sp_flops_event_name "total sp flops"
-#define sp_scalar_flops_event_name "FP_ARITH_INST_RETIRED_SCALAR_SINGLE"
-#define sp_avx_128_flops_event_name "FP_ARITH_INST_RETIRED_128B_PACKED_SINGLE"
-#define sp_avx_256_flops_event_name "FP_ARITH_INST_RETIRED_256B_PACKED_SINGLE"
-
-// flop rates
-#define mflops_metric_name "SP [MFLOP/s]"
-#define mflops_dp_metric_name "DP [MFLOP/s]"
-
-// port usage ratios
-#define port0_usage_ratio "Port0 usage ratio"
-#define port1_usage_ratio "Port1 usage ratio"
-#define port2_usage_ratio "Port2 usage ratio"
-#define port3_usage_ratio "Port3 usage ratio"
-#define port4_usage_ratio "Port4 usage ratio"
-#define port5_usage_ratio "Port5 usage ratio"
-#define port6_usage_ratio "Port6 usage ratio"
-#define port7_usage_ratio "Port7 usage ratio"
-
-// cache volume and bandwidth
-#define l2_bandwidth_metric_name "L2 bandwidth [MBytes/s]"
-#define l2_data_volume_name "L2 data volume [GBytes]"
-#define l2_evict_bandwidth_name "L2D evict bandwidth [MBytes/s]"
-#define l2_evict_data_volume_name "L2D evict data volume [GBytes]"
-#define l2_load_bandwidth_name "L2D load bandwidth [MBytes/s]"
-#define l2_load_data_volume_name "L2D load data volume [GBytes]"
-
-#define l3_bandwidth_metric_name "L3 bandwidth [MBytes/s]"
-#define l3_data_volume_name "L3 data volume [GBytes]"
-#define l3_evict_bandwidth_name "L3 evict bandwidth [MBytes/s]"
-#define l3_evict_data_volume_name "L3 evict data volume [GBytes]"
-#define l3_load_bandwidth_name "L3 load bandwidth [MBytes/s]"
-#define l3_load_data_volume_name "L3 load data volume [GBytes]"
-
-// memory volume and bandwidth
-#define ram_bandwidth_metric_name "Memory bandwidth [MBytes/s]"
-#define ram_data_volume_metric_name "Memory data volume [GBytes]"
-#define ram_evict_bandwidth_name "Memory evict bandwidth [MBytes/s]"
-#define ram_evict_data_volume_name "Memory evict data volume [GBytes]"
-#define ram_load_bandwidth_name "Memory load bandwidth [MBytes/s]"
-#define ram_load_data_volume_name "Memory load data volume [GBytes]"
-
-// other memroy/cache stuff
-#define load_to_store_ratio_metric_name "Load to store ratio"
-
-// ---- End names of things ---- //
 
 // enums for aggregation type and result type
 enum aggregation_type { sum, arithmetic_mean, geometric_mean };
