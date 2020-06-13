5b1a052243b9a07a516175f59f43e616462b7b8e
Riley Weber <rileyw13@protonmail.com>
2020-06-13 16:50:02 -0400

    Now parallel. Made flops/copy self contained

--- tests/papi_minimal_hl.c
+++ tests/papi_minimal_hl.c
@@ -1,11 +1,11 @@
-// compile with something like: 
+// compile with something like:
 // g++ -I/usr/local/papi-master/include -L/usr/local/papi-master/lib papi_minimal_hl.c -o papi_minimal_hl -lpapi
 
 // run with something like:
 // LD_LIBRARY_PATH=/usr/local/papi-master/lib PAPI_OUTPUT_DIRECTORY="data" PAPI_EVENTS="PAPI_DP_OPS,PAPI_L1_TCA" ./papi_minimal_hl
 
-#include <papi.h>
 #include <omp.h>
+#include <papi.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,21 +14,30 @@
 // #define NUM_COPIES 1000
 #define NUM_COPIES 100000
 
-void copy(double * arr, double * copy_arr, size_t n){
-  for (size_t i = 0; i < n; i++){
-    copy_arr[i] = arr[i];
+typedef unsigned long long ull;
+
+void do_copy(double *arr, double *copy_arr, size_t n, ull num_copies) {
+  for (ull iter = 0; iter < num_copies; iter++) {
+    for (size_t i = 0; i < n; i++) {
+      copy_arr[i] = arr[i];
+    }
   }
 }
 
-void handle_error(int retval){
-   printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
-   exit(1);
+double do_flops(double a, double b, double c, ull num_flops) {
+  for (ull i = 0; i < num_flops; i++) {
+    c = a * b + c;
+  }
+  return c;
 }
 
-int main()
-{
-    int retval;
+void handle_error(int retval) {
+  printf("PAPI error %d: %s\n", retval, PAPI_strerror(retval));
+  exit(1);
+}
 
+int main() {
+  int retval;
 
   double a, b, c;
   a = 1.8;
@@ -41,34 +50,33 @@ int main()
 
 #pragma omp parallel
   {
-    for (int j = 0; j < 8; j++)
-    {
-      if(omp_get_thread_num() == 0) printf("iteration %d\n", j);
+    for (int j = 0; j < 8; j++) {
+      if (omp_get_thread_num() == 0)
+        printf("iteration %d\n", j);
 
-    retval = PAPI_hl_region_begin("double_flops");
-    if ( retval != PAPI_OK ) handle_error(1);
+      retval = PAPI_hl_region_begin("double_flops");
+      if (retval != PAPI_OK)
+        handle_error(1);
 
-      if(omp_get_thread_num() == 0) printf("doing flops...\n");
-      for (unsigned long long i = 0; i < NUM_FLOPS; i++)
-      {
-        c = a * b + c;
-      }
+      if (omp_get_thread_num() == 0)
+        printf("doing flops...\n");
+      c = do_flops(a, b, c, NUM_FLOPS);
 
-    retval = PAPI_hl_region_end("double_flops");
-    if ( retval != PAPI_OK ) handle_error(1);
+      retval = PAPI_hl_region_end("double_flops");
+      if (retval != PAPI_OK)
+        handle_error(1);
 
-    retval = PAPI_hl_region_begin("copy");
-    if ( retval != PAPI_OK ) handle_error(1);
+      retval = PAPI_hl_region_begin("copy");
+      if (retval != PAPI_OK)
+        handle_error(1);
 
-      if(omp_get_thread_num() == 0) printf("doing copy operations...\n");
-      for (int i = 0; i < NUM_COPIES; i++){
-        copy(arr, copy_arr, n);
-      }
+      if (omp_get_thread_num() == 0)
+        printf("doing copy operations...\n");
+      do_copy(arr, copy_arr, n, NUM_COPIES);
 
-    retval = PAPI_hl_region_end("copy");
-    if ( retval != PAPI_OK ) handle_error(1);
+      retval = PAPI_hl_region_end("copy");
+      if (retval != PAPI_OK)
+        handle_error(1);
     }
   }
-
 }
-
