78b3f4888807a1cc1d667c8d834b593a1e94c273
Riley Weber <rileyw13@protonmail.com>
2020-06-16 16:57:29 -0400

    now works with multi-threaded code also fixed comments on compilation and usage

--- tests/papi_minimal_ll.c
+++ tests/papi_minimal_ll.c
@@ -1,11 +1,18 @@
 // compile with something like:
-// g++ -I/usr/local/papi-master/include -L/usr/local/papi-master/lib papi_minimal_hl.c -o papi_minimal_hl -lpapi
+// g++ -I/usr/local/papi-master/include -L/usr/local/papi-master/lib papi_minimal_ll.c -o papi_minimal_ll -fopenmp -pthread -lpapi
 
 // run with something like:
-// LD_LIBRARY_PATH=/usr/local/papi-master/lib PAPI_OUTPUT_DIRECTORY="data" PAPI_EVENTS="PAPI_DP_OPS,PAPI_L1_TCA" ./papi_minimal_hl
+// LD_LIBRARY_PATH=/usr/local/papi-master/lib ./papi_minimal_ll
+
+// adding `GOMP_CPU_AFFINITY="0-3"` before call to compiled program may improve
+// stability by pinning threads... but this hasn't been tested yet.
+
+// based closely on example code found at
+// https://bitbucket.org/icl/papi/wiki/PAPI-LL.md 
 
 #include <omp.h>
 #include <papi.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -46,13 +53,7 @@ void print_values(long_long * values, size_t n){
 }
 
 int main() {
-  int retval, ComputationEventSet = PAPI_NULL, MemEventSet = PAPI_NULL;
-  size_t num_values = 4;
-  long_long values[num_values];
-
-  for(size_t i = 0; i < num_values; i++){
-    values[i] = 0;
-  }
+  int retval;
 
   double a, b, c;
   a = 1.8;
@@ -72,56 +73,87 @@ int main() {
     handle_error(retval, "PAPI library initialization error!\n");
   }
 
+#pragma omp parallel
+{
+  unsigned long tid;
+  int thread_retval;
+  int ComputationEventSet = PAPI_NULL;
+  int MemEventSet = PAPI_NULL;
+  int event_code = 0x0;
+  size_t num_values = 4;
+  long_long values[num_values];
+
+  for(size_t i = 0; i < num_values; i++){
+    values[i] = 0;
+  }
+
+  /* initialize multithreading for PAPI */
+  thread_retval = PAPI_thread_init(pthread_self);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While initializing threads");
+  if ((tid = PAPI_thread_id()) == (unsigned long int)-1) 
+    handle_error(1, "Got a bad thread id");
+
   /* Create the Event Set */
-  retval = PAPI_create_eventset(&ComputationEventSet);
-  if (retval != PAPI_OK) handle_error(retval, "While creating event set ComputationEventSet");
+  thread_retval = PAPI_create_eventset(&ComputationEventSet);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While creating event set ComputationEventSet");
 
   /* Add Total Instructions Executed to our Event Set */
-  retval = PAPI_add_event(ComputationEventSet, PAPI_TOT_INS);
-  if (retval != PAPI_OK) handle_error(retval, "While adding PAPI_TOT_INS");
+  thread_retval = PAPI_add_event(ComputationEventSet, PAPI_TOT_INS);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While adding PAPI_TOT_INS");
 
-  retval = PAPI_add_event(ComputationEventSet, PAPI_DP_OPS);
-  if (retval != PAPI_OK) handle_error(retval, "While adding PAPI_DP_OPS");
+  thread_retval = PAPI_add_event(ComputationEventSet, PAPI_DP_OPS);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While adding PAPI_DP_OPS");
 
-  retval = PAPI_create_eventset(&MemEventSet);
-  if (retval != PAPI_OK) handle_error(retval, "While creating event set MemEventSet");
+  // FP_ARITH_INST_RETIRED.SCALAR_DOUBLE
+  // event num: C7H
+  // umask value: 01H
 
-  retval = PAPI_add_event(MemEventSet, PAPI_L2_DCA);
-  if (retval != PAPI_OK) handle_error(retval, "While adding PAPI_L2_DCA");
+  // event_code = 0xC7;
+  // thread_retval = PAPI_enum_event(&event_code, 0x01);
+  // if (thread_retval != PAPI_OK) handle_error(thread_retval, "While running PAPI_enum_event for 0xC7 and 0x01");
 
-  retval = PAPI_add_event(MemEventSet, PAPI_L3_DCA);
-  if (retval != PAPI_OK) handle_error(retval, "While adding PAPI_L3_DCA");
+  // thread_retval = PAPI_add_event(ComputationEventSet, event_code);
+  // if (thread_retval != PAPI_OK) handle_error(thread_retval, "While running PAPI_enum_event for 0xC7 and 0x01");
 
-  retval = PAPI_add_event(MemEventSet, PAPI_MEM_WCY);
-  if (retval != PAPI_OK) handle_error(retval, "While adding PAPI_MEM_WCY");
+  thread_retval = PAPI_create_eventset(&MemEventSet);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While creating event set MemEventSet");
+
+  thread_retval = PAPI_add_event(MemEventSet, PAPI_L2_DCA);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While adding PAPI_L2_DCA");
+
+  thread_retval = PAPI_add_event(MemEventSet, PAPI_L3_DCA);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While adding PAPI_L3_DCA");
+
+  thread_retval = PAPI_add_event(MemEventSet, PAPI_MEM_WCY);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While adding PAPI_MEM_WCY");
 
   // ========= FLOPS ========= //
   printf("\nFLOPS SECTION\n");
   /* Start counting events in the Event Set */
-  retval = PAPI_start(ComputationEventSet);
-  if (retval != PAPI_OK) handle_error(retval, "While starting ComputationEventSet");
+  thread_retval = PAPI_start(ComputationEventSet);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While starting ComputationEventSet");
 
   /* Defined in tests/do_loops.c in the PAPI source distribution */
   do_flops(a, b, c, NUM_FLOPS);
 
   /* Read the counting events in the Event Set */
   // does not reset counters!
-  retval = PAPI_read(ComputationEventSet, values);
-  if (retval != PAPI_OK) handle_error(retval, "While reading ComputationEventSet");
+  thread_retval = PAPI_read(ComputationEventSet, values);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While reading ComputationEventSet");
 
-  printf("After reading the counters:\n");
+  printf("Thread %d, After reading the counters:\n", tid);
   print_values(values, num_values);
 
   /* Reset the counting events in the Event Set */
-  retval = PAPI_reset(ComputationEventSet);
-  if (retval != PAPI_OK) handle_error(retval, "While reseting ComputationEventSet");
+  thread_retval = PAPI_reset(ComputationEventSet);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While reseting ComputationEventSet");
 
   do_flops(a, b, c, NUM_FLOPS);
 
-  retval = PAPI_read(ComputationEventSet, values);
-  if (retval != PAPI_OK) handle_error(retval, "While reading ComputationEventSet");
+  thread_retval = PAPI_read(ComputationEventSet, values);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While reading ComputationEventSet");
 
-  printf("After reading the counters, a second time:\n");
+  printf("Thread %d, After reading the counters, a second time:\n", tid);
   print_values(values, num_values);
   printf("Not doing a reset!\n");
 
@@ -129,58 +161,59 @@ int main() {
   // this will report everything since start
   // also, resets the counters in the case that you do another read or stop
 
-  retval = PAPI_accum(ComputationEventSet, values);
-  if (retval != PAPI_OK) handle_error(retval, "While running PAPI_accum on ComputationEventSet");
+  thread_retval = PAPI_accum(ComputationEventSet, values);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While running PAPI_accum on ComputationEventSet");
 
-  printf("After adding the counters with PAPI_accum:\n");
+  printf("Thread %d, After adding the counters with PAPI_accum:\n", tid);
   print_values(values, num_values);
 
   do_flops(a, b, c, NUM_FLOPS);
 
   /* Stop the counting of events in the Event Set */
-  retval = PAPI_stop(ComputationEventSet, values);
-  if (retval != PAPI_OK) handle_error(retval, "While stopping ComputationEventSet");
+  thread_retval = PAPI_stop(ComputationEventSet, values);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While stopping ComputationEventSet");
 
-  printf("After stopping the counters:\n");
+  printf("Thread %d, after stopping the counters:\n", tid);
   print_values(values, num_values);
 
   // ========= MEM ========= //
   printf("\nMEM SECTION\n");
   /* Start counting events in the Event Set */
-  retval = PAPI_start(MemEventSet);
-  if (retval != PAPI_OK) handle_error(retval, "While starting MemEventSet");
+  thread_retval = PAPI_start(MemEventSet);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While starting MemEventSet");
 
   /* Defined in tests/do_loops.c in the PAPI source distribution */
   do_copy(arr, copy_arr, n, NUM_COPIES);
 
   /* Read the counting events in the Event Set */
-  retval = PAPI_read(MemEventSet, values);
-  if (retval != PAPI_OK) handle_error(retval, "While reading MemEventSet");
+  thread_retval = PAPI_read(MemEventSet, values);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While reading MemEventSet");
 
-  printf("After reading the counters:\n");
+  printf("Thread %d, after reading the counters:\n", tid);
   print_values(values, num_values);
 
   /* Reset the counting events in the Event Set */
-  retval = PAPI_reset(MemEventSet);
-  if (retval != PAPI_OK) handle_error(retval, "While resetting MemEventSet");
+  thread_retval = PAPI_reset(MemEventSet);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While resetting MemEventSet");
 
   do_copy(arr, copy_arr, n, NUM_COPIES);
 
   /* Add the counters in the Event Set */
-  retval = PAPI_accum(MemEventSet, values);
-  if (retval != PAPI_OK) handle_error(retval, "While runing PAPI_accum on MemEventSet");
+  thread_retval = PAPI_accum(MemEventSet, values);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While runing PAPI_accum on MemEventSet");
 
-  printf("After adding the counters:\n");
+  printf("Thread %d, after adding the counters:\n", tid);
   print_values(values, num_values);
 
   do_copy(arr, copy_arr, n, NUM_COPIES);
 
   /* Stop the counting of events in the Event Set */
-  retval = PAPI_stop(MemEventSet, values);
-  if (retval != PAPI_OK) handle_error(retval, "While stopping MemEventSet");
+  thread_retval = PAPI_stop(MemEventSet, values);
+  if (thread_retval != PAPI_OK) handle_error(thread_retval, "While stopping MemEventSet");
 
-  printf("After stopping the counters:\n");
+  printf("Thread %d, after stopping the counters:\n", tid);
   print_values(values, num_values);
+}
 
   return 0;
 }
