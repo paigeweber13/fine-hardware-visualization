d05553c0e5cbd6c60fcd7d5d89f63787b5079fa8
Riley Weber <rileyw13@protonmail.com>
2020-06-24 18:18:03 -0400

    made params variables

--- tests/visualization-color-tests.sh
+++ tests/visualization-color-tests.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# start in root directory of this project
 
 make
 cd examples/polynomial_expansion
@@ -8,25 +9,28 @@ cd ../..
 OLD_FHV_OUTPUT=$FHV_OUTPUT
 OLD_OMP_SCHEDULE=$OMP_SCHEDULE
 
+MEM_HEAVY_PARAMS="67108864 1 800"
+CPU_HEAVY_PARAMS="67108864 1000 80"
+
 echo "running basic polynomial to saturate memory"
 export FHV_OUTPUT="visualizations/polynomial_basic_mem.json"
 export OMP_SCHEDULE="dynamic,8"
-examples/polynomial_expansion/polynomial_fhv_perfmon 67108864 1 800 > /dev/null
+examples/polynomial_expansion/polynomial_fhv_perfmon $MEM_HEAVY_PARAMS > /dev/null
 
 echo "running basic polynomial to saturate cpu"
 export FHV_OUTPUT="visualizations/polynomial_basic_cpu.json"
 export OMP_SCHEDULE="dynamic,8"
-examples/polynomial_expansion/polynomial_fhv_perfmon 67108864 1000 80 > /dev/null
+examples/polynomial_expansion/polynomial_fhv_perfmon $CPU_HEAVY_PARAMS > /dev/null
 
 echo "running optimized polynomial_block to saturate memory"
 export FHV_OUTPUT="visualizations/polynomial_block_mem.json" 
 export OMP_SCHEDULE="dynamic,8"
-examples/polynomial_expansion/polynomial_block_fhv_perfmon 67108864 1 800 > /dev/null
+examples/polynomial_expansion/polynomial_block_fhv_perfmon $MEM_HEAVY_PARAMS > /dev/null
 
 echo "running optimized polynomial_block to saturate cpu"
 export FHV_OUTPUT="visualizations/polynomial_block_cpu.json"
 export OMP_SCHEDULE="dynamic,8"
-examples/polynomial_expansion/polynomial_block_fhv_perfmon 67108864 1000 80 > /dev/null
+examples/polynomial_expansion/polynomial_block_fhv_perfmon $CPU_HEAVY_PARAMS > /dev/null
 
 bin/fhv -v visualizations/polynomial_basic_mem.json -o visualizations/polynomial_basic_mem_128_128_128_227_74_51.svg -c 128 128 128 227 74 51
 bin/fhv -v visualizations/polynomial_basic_cpu.json -o visualizations/polynomial_basic_cpu_128_128_128_227_74_51.svg -c 128 128 128 227 74 51
@@ -61,4 +65,3 @@ bin/fhv -v visualizations/polynomial_block_cpu.json -o visualizations/polynomial
 # cleanup
 export FHV_OUTPUT=$OLD_FHV_OUTPUT
 export OMP_SCHEDULE=$OLD_FHV_OUTPUT
-
