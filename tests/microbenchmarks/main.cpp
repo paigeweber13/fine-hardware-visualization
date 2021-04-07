b3fe667d88f517ac3c190cf85d4e641abdd51714
Riley Weber <rileyw13@protonmail.com>
2021-04-07 14:41:32 -0400

    timed benchmark manually

--- tests/microbenchmarks/main.cpp
+++ tests/microbenchmarks/main.cpp
@@ -1,9 +1,12 @@
 // third-party imports
-// #include <stdio.h>
+#include <chrono>
+#include <iostream>
 // #include <stdlib.h>
+// #include <stdio.h>
 
 // first-party imports
 #include <fhv_perfmon.hpp>
+#include <test_types.h>
 // #include <testcases.h>
 
 /* 
@@ -29,23 +32,6 @@
  * } TestCase;
  * 
  * 
- * Kernels we're interested in, taken from likwid/bench/GCC/testcases.h. Each
- * of these is a struct of type TestCase:
- *
- * {"copy_avx" , STREAM_2, DOUBLE, 16, &copy_avx, 0, 16, "Double-precision
- *   vector copy, optimized for AVX", 1, 1, -1, 16, 11, 14},
- * {"load_avx" , STREAM_1, DOUBLE, 16, &load_avx, 0, 8, "Double-precision load,
- *   optimized for AVX", 1, 0, -1, 16, 7, 6},
- * {"store_avx" , STREAM_1, DOUBLE, 16, &store_avx, 0, 8, "Double-precision
- *   store, optimized for AVX", 0, 1, -1, 20, 7, 10},
- * {"peakflops_avx_fma" , STREAM_1, DOUBLE, 4, &peakflops_avx_fma, 30, 8,
- *   "Double-precision multiplications and additions with a single load,
- *   optimized for AVX FMAs", 1, 0, -1, 32, 19, 18},
- * {"peakflops_sp_avx_fma" , STREAM_1, SINGLE, 8, &peakflops_sp_avx_fma, 30, 4,
- *   "Single-precision multiplications and additions with a single load,
- *   optimized for AVX FMAs", 1, 0, -1, 32, 19, 18},
- *
- * 
  * seems to be possible to execute these functions with func(size, stream)
  * where size is an unsigned long long and stream is a pointer to memory
  *
@@ -53,9 +39,44 @@
 
 typedef unsigned long long ull;
 
-#define EXECUTE(func) func;
+// Kernels we're interested in, taken from likwid/bench/GCC/testcases.h
 
 extern "C" void peakflops_avx_fma(ull, double*);
+extern "C" void peakflops_sp_avx_fma(ull, double*);
+
+#define NUMKERNELS 5
+
+#define peakflops_sp_avx_fma_kernel 4
+
+static const TestCase kernels[NUMKERNELS] = {
+  // {"copy_avx" , STREAM_2, DOUBLE, 16, &copy_avx, 0, 16, "Double-precision "
+  //   "vector copy, optimized for AVX", 1, 1, -1, 16, 11, 14},
+  // {"load_avx" , STREAM_1, DOUBLE, 16, &load_avx, 0, 8, "Double-precision load,"
+  //   " optimized for AVX", 1, 0, -1, 16, 7, 6},
+  // {"store_avx" , STREAM_1, DOUBLE, 16, &store_avx, 0, 8, "Double-precision "
+  //   "store, optimized for AVX", 0, 1, -1, 20, 7, 10},
+  // {"peakflops_avx_fma" , STREAM_1, DOUBLE, 4, &peakflops_avx_fma, 30, 8,
+  //   "Double-precision multiplications and additions with a single load, "
+  //   "optimized for AVX FMAs", 1, 0, -1, 32, 19, 18},
+  {"peakflops_sp_avx_fma" , STREAM_1, SINGLE, 8, NULL, 30, 4,
+    "Single-precision multiplications and additions with a single load, "
+    "optimized for AVX FMAs", 1, 0, -1, 32, 19, 18},
+};
+
+void peakflops_manual(ull num_i, ull n, double* array) {
+  auto kernel = kernels[peakflops_sp_avx_fma_kernel];
+
+  auto start = std::chrono::steady_clock::now();
+  for (ull i = 0; i < num_i; i++) {
+    peakflops_sp_avx_fma(n, array);
+  }
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> duration = end-start;
+  std::cout << "Size: " << n << ", Duration (s): " << duration.count()
+    << "FLOP/s: " 
+    << (double)num_i * (double)n * (double)kernels->flops / duration.count()
+    << std::endl;
+}
 
 int main(int argc, char** argv) {
 
@@ -68,8 +89,12 @@ int main(int argc, char** argv) {
 
   // c++
 
+  // n above ~10,000 cause segfault?
   const ull n = 8192;
+  const ull num_i = 1000000;
   double *array = new double[n];
-  peakflops_avx_fma(n, array);
+
+  peakflops_manual(num_i, n, array);
+
   delete[] array;
 }
