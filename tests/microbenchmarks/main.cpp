085012ebb4ea1846f620f4e33d1502be52ab7394
Riley Weber <rileyw13@protonmail.com>
2021-04-14 15:47:58 -0400

    Fixed segfaults, many improvements
    Added code that measures with FHV, added manual parallel code.

--- tests/microbenchmarks/main.cpp
+++ tests/microbenchmarks/main.cpp
@@ -41,29 +41,60 @@ typedef unsigned long long ull;
 
 // Kernels we're interested in, taken from likwid/bench/GCC/testcases.h
 
+extern "C" void copy_avx();
+extern "C" void load_avx();
+extern "C" void store_avx();
 extern "C" void peakflops_avx_fma(ull, double*);
-extern "C" void peakflops_sp_avx_fma(ull, double*);
+extern "C" void peakflops_sp_avx_fma(ull, float*);
 
 #define NUMKERNELS 5
 
 #define peakflops_sp_avx_fma_kernel 4
 
 static const TestCase kernels[NUMKERNELS] = {
-  // {"copy_avx" , STREAM_2, DOUBLE, 16, &copy_avx, 0, 16, "Double-precision "
-  //   "vector copy, optimized for AVX", 1, 1, -1, 16, 11, 14},
-  // {"load_avx" , STREAM_1, DOUBLE, 16, &load_avx, 0, 8, "Double-precision load,"
-  //   " optimized for AVX", 1, 0, -1, 16, 7, 6},
-  // {"store_avx" , STREAM_1, DOUBLE, 16, &store_avx, 0, 8, "Double-precision "
-  //   "store, optimized for AVX", 0, 1, -1, 20, 7, 10},
-  // {"peakflops_avx_fma" , STREAM_1, DOUBLE, 4, &peakflops_avx_fma, 30, 8,
-  //   "Double-precision multiplications and additions with a single load, "
-  //   "optimized for AVX FMAs", 1, 0, -1, 32, 19, 18},
-  {"peakflops_sp_avx_fma" , STREAM_1, SINGLE, 8, NULL, 30, 4,
-    "Single-precision multiplications and additions with a single load, "
-    "optimized for AVX FMAs", 1, 0, -1, 32, 19, 18},
+  {(char*)"copy_avx" , STREAM_2, DOUBLE, 16, NULL, 0, 16, 
+    (char*)"Double-precision vector copy, optimized for AVX", 1, 1, -1, 16,
+    11, 14},
+  {(char*)"load_avx" , STREAM_1, DOUBLE, 16, NULL, 0, 8, 
+    (char*)"Double-precision load, optimized for AVX", 1, 0, -1, 16, 7, 6},
+  {(char*)"store_avx" , STREAM_1, DOUBLE, 16, NULL, 0, 8, 
+    (char*)"Double-precision store, optimized for AVX", 0, 1, -1, 20, 7, 10},
+  {(char*)"peakflops_avx_fma" , STREAM_1, DOUBLE, 4, NULL, 30, 8,
+    (char*)"Double-precision multiplications and additions with a single "
+    "load, optimized for AVX FMAs", 1, 0, -1, 32, 19, 18},
+  {(char*)"peakflops_sp_avx_fma" , STREAM_1, SINGLE, 8, NULL, 30, 4,
+    (char*)"Single-precision multiplications and additions with a single "
+    "load, optimized for AVX FMAs", 1, 0, -1, 32, 19, 18},
 };
 
-void peakflops_manual(ull num_i, ull n, double* array) {
+void peakflops_manual_parallel(ull num_i, ull n, float* array) {
+  auto kernel = kernels[peakflops_sp_avx_fma_kernel];
+
+  unsigned num_procs;
+  #pragma omp parallel
+  {
+    #pragma omp single
+    {
+      num_procs = omp_get_num_threads();
+    }
+  }
+
+  auto start = std::chrono::steady_clock::now();
+  #pragma omp parallel for
+  for (ull i = 0; i < num_i; i++) {
+    peakflops_sp_avx_fma(n, array);
+  }
+  auto end = std::chrono::steady_clock::now();
+  std::chrono::duration<double> duration = end-start;
+
+  double flops = (double)num_procs * (double)num_i * (double)n * 
+    (double)kernel.flops / duration.count();
+
+  std::cout << "Size: " << n << ", Duration (s): " << duration.count()
+    << " FLOP/s: " << flops << std::endl;
+}
+
+void peakflops_manual(ull num_i, ull n, float* array, bool parallel=false) {
   auto kernel = kernels[peakflops_sp_avx_fma_kernel];
 
   auto start = std::chrono::steady_clock::now();
@@ -72,29 +103,71 @@ void peakflops_manual(ull num_i, ull n, double* array) {
   }
   auto end = std::chrono::steady_clock::now();
   std::chrono::duration<double> duration = end-start;
+
+  double flops = (double)num_i * (double)n * (double)kernel.flops / 
+    duration.count();
+
   std::cout << "Size: " << n << ", Duration (s): " << duration.count()
-    << "FLOP/s: " 
-    << (double)num_i * (double)n * (double)kernels->flops / duration.count()
-    << std::endl;
+    << " FLOP/s: " << flops << std::endl;
+}
+
+void peakflops_fhv(ull num_i, ull n, float* array) {
+
+  fhv_perfmon::init("", "peakflops_sp_avx_fma");
+
+  if (num_i < 10) {
+    std::cout << "ERROR: in peakflops_fhv: num_i must be >= 10"
+      << std::endl;
+    return;
+  }
+  num_i /= 10;
+
+  for (ull i = 0; i < 10; i++) {
+    fhv_perfmon::startRegion("peakflops_sp_avx_fma");
+    peakflops_sp_avx_fma(n, array);
+    fhv_perfmon::stopRegion("peakflops_sp_avx_fma");
+    fhv_perfmon::nextGroup();
+  }
+
+  fhv_perfmon::close();
+  fhv_perfmon::printAggregateResults();
+  const unsigned BYTES_PER_FLOAT = 4;
+  std::string paramString = "array n: " + std::to_string(n) + 
+    " array size bytes: " + std::to_string(n * BYTES_PER_FLOAT) +
+    " num iterations: " + std::to_string(num_i);
+  fhv_perfmon::resultsToJson(paramString);
 }
 
 int main(int argc, char** argv) {
+  // if (argc < 3) {
+  //   std::cout << "Usage: " << argv[0] << " measurement_type array_n num_iter" 
+  //     << std::endl;
+  //   std::cout << "       " << "measurement_type must be one of 'none', "
+  //     << "'manual', manual_parallel', 'fhv', or 'fhv_parallel'."
+  //     << std::endl;
+  //   return 0;
+  // }
 
-  // c
+  const ull max_n = 1000000001;
+  ull num_i;
+  float *array = (float*)aligned_alloc(64, max_n * sizeof(float));
 
-  // const ull n = 1024;
-  // double *array = (double*)malloc(n * sizeof(double));
-  // peakflops_avx_fma(n, array);
-  // free(array);
+  num_i = 100000000;
+  for (ull n = 100; n < max_n; n *= 10) {
+    peakflops_manual(num_i, n, array);
+    num_i /= 10;
+  }
 
-  // c++
+  num_i = 100000000;
+  for (ull n = 100; n < max_n; n *= 10) {
+    peakflops_manual_parallel(num_i, n, array);
+    num_i /= 10;
+  }
 
-  // n above ~10,000 cause segfault?
-  const ull n = 8192;
-  const ull num_i = 1000000;
-  double *array = new double[n];
+  num_i = 100000000;
+  ull n = 100;
+  peakflops_fhv(num_i, n, array);
 
-  peakflops_manual(num_i, n, array);
-
-  delete[] array;
+  free(array);
 }
+
