92e8a659246a5621391c0f46dd27458991c58c5a
Riley Weber <rileyw13@protonmail.com>
2021-05-13 00:18:21 -0400

    improved code quality

--- examples/minimal/fhv_minimal.cpp
+++ examples/minimal/fhv_minimal.cpp
@@ -29,31 +29,37 @@ int main()
   printf("\nThis is a minimal example of how the fhv fhv_perfmon works"
          "\n");
 
-  // fhv_perfmon::init("double_flops,copy");
-  fhv_perfmon::init();
-
   double a, b, c;
   a = 1.8;
   b = 3.2;
   c = 0.0;
 
   const lli n = 2048;
+  const int NUM_FHV_GROUPS = 7;
+  const std::string REGION_FLOPS = "double_flops";
+  const std::string REGION_COPY = "copy";
+
   double arr[n];
   double copy_arr[n];
 
+  // optional: specify regions ahead of time. This is recommended, as it
+  // removes the risk of overhead on the first call to `startRegion`
+  // fhv_perfmon::init((REGION_FLOPS + "," + REGION_COPY).c_str());
+  fhv_perfmon::init();
+
 #pragma omp parallel
   {
-    for (int j = 0; j < 8; j++)
+    for (int j = 0; j < NUM_FHV_GROUPS; j++)
     {
       printf("thread %d, iteration %d\n", omp_get_thread_num(), j);
 
-      fhv_perfmon::startRegion("double_flops");
+      fhv_perfmon::startRegion(REGION_FLOPS.c_str());
       c = do_flops(a, b, c, NUM_FLOPS);
-      fhv_perfmon::stopRegion("double_flops");
+      fhv_perfmon::stopRegion(REGION_FLOPS.c_str());
 
-      fhv_perfmon::startRegion("copy");
+      fhv_perfmon::startRegion(REGION_COPY.c_str());
       do_copy(arr, copy_arr, n, NUM_COPIES);
-      fhv_perfmon::stopRegion("copy");
+      fhv_perfmon::stopRegion(REGION_COPY.c_str());
 
       fhv_perfmon::nextGroup();
     }
