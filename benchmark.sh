10e0826f4518ba8cf886e10cca6549ee2d2d165d
Paige Weber <paigeweber31@gmail.com>
2021-06-28 14:37:13 -0400

    prints exact command run

--- benchmark.sh
+++ benchmark.sh
@@ -111,10 +111,7 @@ FLOPS_LIKWID_GROUP=( FLOPS_DP   FLOPS_SP)
 # params expected: test simple name, bench name, likwid group, size, num iter
 # for example: Read/write bandwidth, copy_avx, L2, 10kB, 2000000
 run_likwid_bench_test () { 
-    echo
-    echo
     echo "Test $2 ($1) for group $3. Size: $4, num_iter: $5."
-    echo
   
     SECONDS=0
   
@@ -128,11 +125,14 @@ run_likwid_bench_test () {
     #    | grep -B 1 -A 20 -E "Metric.*Sum" # comment this last line if you don't want to exclude benchmark info
   
     # Measurement with likwid-bench internal tracking
-    $LIKWID_PATH/bin/likwid-bench -t $2 -i $5 -w S0:$4:$NUM_CORES
+    echo "Running command '$LIKWID_PATH/bin/likwid-bench -t $2 -i $5 -w S0:$4:$NUM_CORES'"
+    $LIKWID_PATH/bin/likwid-bench -t $2 -i $5 -w S0:$4:$NUM_CORES \
+      | grep -E 'Test|MByte/s|MFlop/s|elapsed'
   
     # time
     echo "$(($SECONDS / 3600))h $((($SECONDS / 60) % 60))m $(($SECONDS % 60))s"\
       " elapsed."
+    echo
 }
 
 # TODO: should just use sed to isolate values and create the machine-stats json
@@ -142,8 +142,7 @@ for test_num in $(seq 0 $(($BW_NUM_TESTS - 1)) ); do
   for level in $(seq 0 $(($BW_NUM_LEVELS-1)) ); do
     run_likwid_bench_test "${BW_TESTS[$test_num]}" \
       "${BW_LIKWID_TEST_NAME[$test_num]}" "${BW_LIKWID_GROUP[$level]}" \
-      "${BW_SIZES[$level]}" "${BW_ITERS[$level]}" \
-      | grep -E 'Test|MByte/s|elapsed'
+      "${BW_SIZES[$level]}" "${BW_ITERS[$level]}"
     echo
   done
 done
@@ -152,8 +151,7 @@ done
 for test_num in $(seq 0 $(($FLOPS_NUM_TESTS - 1)) ); do
   run_likwid_bench_test "${FLOPS_TESTS[$test_num]}" \
     "${FLOPS_LIKWID_TEST_NAME[$test_num]}" "${FLOPS_LIKWID_GROUP[$test_num]}" \
-    "${FLOPS_SIZES[$test_num]}" "${FLOPS_ITERS[$test_num]}" \
-      | grep -E 'Test|MFlop/s|elapsed'
+    "${FLOPS_SIZES[$test_num]}" "${FLOPS_ITERS[$test_num]}"
   echo
 done
 
